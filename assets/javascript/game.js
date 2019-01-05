e5066d079cdc6c7462424744ffc6196283468621
czops <37029225+czops@users.noreply.github.com>
2019-01-05 12:24:34 -0600

    the application works, can still stylize this

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 
-var RandomNumber = 0;
-//var PlayerSum = 0;
+var RandomNumber = 1;
+var PlayerSum = 0;
 var PlayerGuess = 0;
+//adding this so that you can jQuery the 1-4 crystal values into this variable
+var CrystalValue = 0;
 
 var Crystal1 = 0;
 var Crystal2 = 0;
@@ -10,24 +12,27 @@ var Crystal3 = 0;
 var Crystal4 = 0;
 
 var Wins = 0;
-var Loses = 0;
+var Losses = 0;
 
 var StartGame = function(){
     //Call this function to pick out a random number and edit the HTML element
     min = Math.ceil(19);
     max = Math.floor(120);
-    var RandomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-
+    RandomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    debugger;
     $("#RandomNumber").text(RandomNumber);
+    $("#PlayerSum").text(PlayerSum);
+    $("#Wins").text("Wins: " + Wins);
+    $("#Losses").text("Losses: " + Losses);
 
     //Pickout random numbers for each off the Crystals and assign the values to the crystal buttons
     
     minCrystal = Math.ceil(1);
     maxCrystal = Math.floor(12);
-    var Crystal1 = Math.floor(Math.random() * (maxCrystal - minCrystal + 1)) + minCrystal;
-    var Crystal2 = Math.floor(Math.random() * (maxCrystal - minCrystal + 1)) + minCrystal;
-    var Crystal3 = Math.floor(Math.random() * (maxCrystal - minCrystal + 1)) + minCrystal;
-    var Crystal4 = Math.floor(Math.random() * (maxCrystal - minCrystal + 1)) + minCrystal;
+    Crystal1 = Math.floor(Math.random() * (maxCrystal - minCrystal + 1)) + minCrystal;
+    Crystal2 = Math.floor(Math.random() * (maxCrystal - minCrystal + 1)) + minCrystal;
+    Crystal3 = Math.floor(Math.random() * (maxCrystal - minCrystal + 1)) + minCrystal;
+    Crystal4 = Math.floor(Math.random() * (maxCrystal - minCrystal + 1)) + minCrystal;
 
 
     $("#button-1").val(Crystal1);
@@ -39,15 +44,39 @@ var StartGame = function(){
 
 
 //When a crystal is pressed, call this function to update the Player Sum element by adding the crystal's random value to this Player sum number
+
+
+
 $(".crystal").on("click", function() {
-    PlayerSum += $(this).val();
-$("#PlayerSum").text(PlayerSum);
+    var CrystalValue = ($(this).attr("value"));
+    xtal = parseInt(CrystalValue)
+    PlayerSum += xtal;
+
+    $("#PlayerSum").text(PlayerSum);
+
+    WinCondition();
+
 });
 
 
 var WinCondition = function(){
     //After the PlayerSum is updated, check if the Player has won or lost.
-
+    if (PlayerSum === RandomNumber){
+        debugger;
+        console.log("You win!");
+        PlayerSum = 0;
+        Wins++;
+        StartGame();
+        
+    }
+    
+    if (PlayerSum > RandomNumber) {
+        debugger;
+        console.log("You Lose!");
+        PlayerSum = 0;
+        Losses++;
+        StartGame();
+    }
     //Update the Wins or Loses correspondingly
 
 };
